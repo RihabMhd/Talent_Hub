488df46cd50bb83067f92e08c231b36c2ef2e3b7
RihabMhd <rihabmahdi19@gmail.com>
2026-01-22 00:06:22 +0100

    refactor: update admin routes to use admin-specific controllers

--- routes/admin.php
+++ routes/admin.php
@@ -14,7 +14,7 @@ return function (Router $router, $controllers, $middlewares) {
         
         // Dashboard
         $router->get('/dashboard', function() use ($controllers) {
-            $controllers['dashboard']->index();
+            $controllers['adminDashboard']->index();
         });
         
         // --- User Management ---
@@ -77,79 +77,59 @@ return function (Router $router, $controllers, $middlewares) {
         });
         
         // --- Job Offers Management ---
-        // [FIX] Changed 'job' to 'jobOffer' to match index.php
         $router->get('/jobs', function() use ($controllers) {
-            $controllers['jobOffer']->index();
+            $controllers['adminJobOffer']->index();
         });
         
         $router->get('/jobs/create', function() use ($controllers) {
-            $controllers['jobOffer']->create();
+            $controllers['adminJobOffer']->create();
         });
         
         $router->post('/jobs', function() use ($controllers) {
-            $controllers['jobOffer']->store();
+            $controllers['adminJobOffer']->store();
         });
         
         $router->get('/jobs/{id}/edit', function($id) use ($controllers) {
-            $controllers['jobOffer']->edit($id);
+            $controllers['adminJobOffer']->edit($id);
         });
         
         $router->post('/jobs/{id}', function($id) use ($controllers) {
-            $controllers['jobOffer']->update($id);
+            $controllers['adminJobOffer']->update($id);
         });
         
-        // [FIX] Changed to match the archive method signature
         $router->post('/jobs/{id}/archive', function($id) use ($controllers) {
-            $controllers['jobOffer']->archive($id);
+            $controllers['adminJobOffer']->archive($id);
         });
         
         $router->post('/jobs/{id}/restore', function($id) use ($controllers) {
-            $controllers['jobOffer']->restore($id);
+            $controllers['adminJobOffer']->restore($id);
         });
         
         $router->post('/jobs/{id}/delete', function($id) use ($controllers) {
-            $controllers['jobOffer']->destroy($id);
+            $controllers['adminJobOffer']->destroy($id);
         });
         
         // --- Applications Management ---
-        // [FIX] Changed 'application' to 'applications' (plural) to match index.php
         $router->get('/applications', function() use ($controllers) {
-            $controllers['applications']->index();
+            $controllers['adminApplications']->index();
         });
         
-        // [FIX] Updated to use blockCandidate/unblockCandidate methods
         $router->get('/applications/block/{id}', function($id) use ($controllers) {
-            $controllers['applications']->blockCandidate($id);
+            $controllers['adminApplications']->blockCandidate($id);
         });
         
         $router->get('/applications/unblock/{id}', function($id) use ($controllers) {
-            $controllers['applications']->unblockCandidate($id);
+            $controllers['adminApplications']->unblockCandidate($id);
         });
         
         // --- Statistics ---
         $router->get('/statistics', function() use ($controllers) {
-            $controllers['statistics']->index();
+            $controllers['adminStatistics']->index();
         });
         
         $router->get('/statistics/export', function() use ($controllers) {
-            $controllers['statistics']->export();
+            $controllers['adminStatistics']->export();
         });
         
-        // --- Roles Management ---
-        $router->get('/roles', function() use ($controllers) {
-            $controllers['role']->index();
-        });
-        
-        $router->post('/roles/store', function() use ($controllers) {
-            $controllers['role']->store();
-        });
-        
-        $router->post('/roles/update/{id}', function($id) use ($controllers) {
-            $controllers['role']->update($id);
-        });
-        
-        $router->post('/roles/delete/{id}', function($id) use ($controllers) {
-            $controllers['role']->destroy($id);
-        });
     });
 };
